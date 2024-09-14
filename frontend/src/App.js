2de4c19081211a6a5d7a9a07d89d53fd6acba70d
unknown <deepthishalini603@gmail.comgit config --global user.name>
2024-09-14 09:51:52 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'; 
+// import StudentTable from './Components/StudentTable.js'; 
+import StudentTable from './Components/StudentTable';
+function App() { 
+  return ( 
+    <div className="App"> 
+      <StudentTable/> 
+      
+    </div> 
+  ); 
+} 
+ 
+export default App;
